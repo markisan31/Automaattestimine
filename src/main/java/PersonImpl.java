7fc9603e388237b04918c169c8420c29a509bc79
Mark Kalda <mark.kalda94@gmail.com>
2020-04-18 15:37:27 +0300

    First commit

---
+++ src/main/java/PersonImpl.java
@@ -0,0 +1,28 @@
+public class PersonImpl implements Person {
+
+
+    public String getFullName() {
+        return null;
+    }
+
+    public String getFirstName() {
+        return null;
+    }
+
+    public String getPreferredName() {
+        return null;
+    }
+
+    public String getDateOfBirth() {
+        return null;
+    }
+
+    public int getAge() {
+        return 0;
+    }
+
+
+    public void sayHello() {
+
+    }
+}
