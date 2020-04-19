61d1a0d1f47fb7ab334323b2af46fada3d081c0a
Mark Kalda <mark.kalda94@gmail.com>
2020-04-19 23:20:09 +0300

    first commit

---
+++ src/main/java/Course.java
@@ -0,0 +1,33 @@
+public class Course {
+
+    public String courseName;
+    public int sumOfEAP;
+    public int maxStudentsQuantity;
+    public int startDate;
+    public int endDate;
+    public Teacher teacher;
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public int getSumOfEAP() {
+        return sumOfEAP;
+    }
+
+    public int getMaxStudentsQuantity() {
+        return maxStudentsQuantity;
+    }
+
+    public int getStartDate() {
+        return startDate;
+    }
+
+    public int getEndDate() {
+        return endDate;
+    }
+
+    public Teacher getTeacher() {
+        return teacher;
+    }
+}
---
+++ src/main/java/Utils.java
@@ -0,0 +1,12 @@
+import java.util.List;
+
+public class Utils {
+
+    public static void printAllStudents(List<Student> students) {
+
+        for (Student student : students) {
+            System.out.println(student.getFullName());
+        }
+
+    }
+}
